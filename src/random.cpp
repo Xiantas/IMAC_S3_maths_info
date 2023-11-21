1abdca338f899e64a04ad5cbef84936f811176f6
SegoleneLazarus <seg.laz@ik.me>
2023-11-21 20:10:59 +0100

    random

--- src/random.cpp
+++ src/random.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 #include <vector>
-#include <ctime>
+#include <random>
+#include <chrono>
 
 #include <Eigen/Dense>
 
 #include "Geogebra_conics.hpp"
+
+unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+
+std::cout << std::endl << "std::mt19937" << std::endl;
+std::mt19937 generator(seed);
+
+std::cout << std::endl << "uniform int distribution (1,6)" << std::endl;
+std::uniform_int_distribution<double> uniformIntDistribution(-10,10);
+std::cout << std::endl;
 
 Eigen::VectorXd randomPt(){
     srand(time(NULL));
